05e7880f05ff55278003d90e53e745d6f8cf435f
Daisuke Horiuchi <dhoriuchi001@gmail.com>
2023-05-20 14:55:28 -0700

    [Add] experiences page

--- src/experience.jsx
+++ src/experience.jsx
@@ -1,33 +1,45 @@
 import React, { useState } from 'react';
+import { Button, Form, FormGroup, FormLabel, FormControl } from 'react-bootstrap';
 
-const CommentForm = () => {
-    const [comment, setComment] = useState('');
+const ExperienceForm = () => {
+  const [comment, setComment] = useState('');
+  const [posts, setPosts] = useState([]);
 
-    const handleChange = (event) => {
-        setComment(event.target.value);
-    }
+  const handleChange = (event) => {
+    setComment(event.target.value);
+  }
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(comment);
-        setComment('');
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setPosts([...posts, comment]);
+    setComment('');
+  }
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="comment">
-                    Comment: 
-                    <textarea value={comment} onChange={handleChange} id="comment" />
-                </label>
-                <button type="submit">Post Comment</button>
-            </form>
-            <div>
-                <h2>Your Comment:</h2>
-                <p>{comment}</p>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <Form onSubmit={handleSubmit}>
+        <h2>Your Experience:</h2>
+        <FormGroup>
+          <FormControl
+            as="textarea"
+            name="comment"
+            id="comment"
+            value={comment}
+            onChange={handleChange}
+          />
+        </FormGroup>
+        <Button type="submit">Post Comment</Button>
+      </Form>
+      <div>
+        <h2>Your Posts:</h2>
+        {posts.map((post, index) => (
+          <div key={index}>
+            <p>{post}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
-export default CommentForm;
+export default ExperienceForm;
